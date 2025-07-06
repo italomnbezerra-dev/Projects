8b3b77cf89657bbed7e864dca71ec73eba5b1a95
italomnbezerra-dev <italomnbezerra@gmail.com>
2025-07-05 21:27:21 -0300

    CalulaBask.py

--- CalulaBask.py
+++ CalulaBask.py
@@ -1,22 +1,22 @@
 import math
-qname = input('Olá, como você se chama?')
-print('Óla,', qname, 'seja bem vindo ao CalculaBask')
-numA = float(input('Digite aqui o A da sua equação: '))
-if numA == 0:
+nome_usuario = input('Olá, como você se chama?')
+print('Óla,', nome_usuario, 'seja bem vindo ao CalculaBask')
+num_A = float(input('Digite aqui o A da sua equação: '))
+if num_A == 0:
     print('O valor de A deve ser diferente de Zero.')
 else:
-    numB = float(input('Digite aqui o B da sua equação: '))
-    numC = float(input('Digite aqui o C da sua equação: '))
-    numDelta = (numB ** 2) - 4 * numA * numC
-    print('O delta da sua equação é:', numDelta)
-    if numDelta < 0:
+    num_B = float(input('Digite aqui o B da sua equação: '))
+    num_C = float(input('Digite aqui o C da sua equação: '))
+    num_Delta = (num_B ** 2) - 4 * num_A * num_C
+    print('O delta da sua equação é:', num_Delta)
+    if num_Delta < 0:
         print('Delta não possui resultados reais')
-    elif numDelta == 0:
+    elif num_Delta == 0:
         print('A solução possui apenas uma solução real')
-        numX1 = (- numB + numDelta ** (1/2)) / (2 * numA)
-        numX2 = (- numB - numDelta ** (1/2)) / (2 * numA)
-        print(f'X1 é igual à', int(numX1), 'e o X2 é igual à', int(numX2))
+        num_X1 = (- num_B + num_Delta ** (1/2)) / (2 * num_A)
+        num_X2 = (- num_B - num_Delta ** (1/2)) / (2 * num_A)
+        print(f'X1 é igual à', int(num_X1), 'e o X2 é igual à', int(num_X2))
     else:
-        numX1 = (- numB + numDelta ** (1/2)) / (2 * numA)
-        numX2 = (- numB - numDelta ** (1/2)) / (2 * numA)
-        print(f'X1 é igual à', int(numX1), 'e o X2 é igual à', int(numX2))
+        num_X1 = (- num_B + num_Delta ** (1/2)) / (2 * num_A)
+        num_X2 = (- num_B - num_Delta ** (1/2)) / (2 * num_A)
+        print(f'X1 é igual à', int(num_X1), 'e o X2 é igual à', int(num_X2))
